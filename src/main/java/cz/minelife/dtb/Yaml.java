5bfc1e0602dc1fcb49e54ee660df075fd360ef95
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-14 11:34:43 +0100

    refactor(Yaml) code cleanup

--- src/main/java/cz/minelife/dtb/Yaml.java
+++ src/main/java/cz/minelife/dtb/Yaml.java
@@ -7,18 +7,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Yaml {
     private JavaPlugin plugin;
     private FileConfiguration config;
+
     public Yaml(JavaPlugin plugin) {
         this.plugin = plugin;
         this.config = plugin.getConfig();
     }
+
     private void reloadConfig() {
         this.plugin.saveConfig();
         this.config = plugin.getConfig();
     }
+
     public void setPlayerLives(Player p, int lives) {
         this.config.set("players." + p.getDisplayName() + ".lives", lives);
         this.reloadConfig();
     }
+
     public int getPlayerLives(Player p) {
         return this.config.getInt("players." + p.getDisplayName() + ".lives");
     }
