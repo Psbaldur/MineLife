b170202f021a3d079997d0ade1cf5ff3ef20b5d8
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-14 19:16:02 +0100

    feat(Boogeyman) setup

--- src/main/java/cz/minelife/lifes/Boogeyman.java
+++ src/main/java/cz/minelife/lifes/Boogeyman.java
@@ -1,5 +1,13 @@
 package cz.minelife.lifes;
 
-public class Boogeyman {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
+public class Boogeyman implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
 }
