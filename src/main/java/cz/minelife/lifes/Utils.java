824e2854c101536c03fde9199828ca1195f469d8
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-15 11:22:24 +0100

    feat(Utils): Added subtext and randomPlayer

--- src/main/java/cz/minelife/lifes/Utils.java
+++ src/main/java/cz/minelife/lifes/Utils.java
@@ -1,8 +1,10 @@
 package cz.minelife.lifes;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.Collection;
 import java.util.Random;
 
 public class Utils {
@@ -28,11 +30,19 @@ public class Utils {
         }
     }
 
-    public static void setTitle(Player player, String text) {
-        player.sendTitle(ChatColor.translateAlternateColorCodes('&', "&c" + text), "", 1, 20*5, 1);
+    public static void setTitle(Player player, String text, String subText) {
+        player.sendTitle(ChatColor.translateAlternateColorCodes('&', text), ChatColor.translateAlternateColorCodes('&', subText), 1, 20*5, 1);
     }
 
     public static int randomInt(int max, int min) {
         return RANDOM.nextInt(max - min) + min;
+    }
+
+    public static Player randomPlayer() {
+        Player[] players = Bukkit.getOnlinePlayers().toArray(new Player[0]);
+        int randomNumberInRange = RANDOM.nextInt((players.length - 1));
+        Player p = players[randomNumberInRange];
+
+        return p;
     }
 }
