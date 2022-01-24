6c8a62a7ff3f59fa4e8076a086dab849524b33bd
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-24 13:46:09 +0100

    refactor(Events): Added events

--- src/main/java/cz/minelife/Events.java
+++ src/main/java/cz/minelife/Events.java
@@ -1,10 +1,18 @@
 package cz.minelife;
 
-import cz.minelife.players.onDeath;
+import cz.minelife.players.handlers.OnDeath;
+import cz.minelife.players.handlers.OnPlayerChat;
+import cz.minelife.players.handlers.OnPlayerJoin;
 
 public class Events {
     public Events(Main main) {
         //onDeath Listener
-        main.getServer().getPluginManager().registerEvents(new onDeath(), main);
+        main.getServer().getPluginManager().registerEvents(new OnDeath(main), main);
+
+        //onPlayerChat Listener
+        main.getServer().getPluginManager().registerEvents(new OnPlayerChat(), main);
+
+        //onPlayerJoin Listener
+        main.getServer().getPluginManager().registerEvents(new OnPlayerJoin(main), main);
     }
 }
