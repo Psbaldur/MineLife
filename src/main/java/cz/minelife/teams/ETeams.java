3896c38bb9b16bba443979a6f895de5e22f68a73
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-24 15:30:58 +0100

    refactor(ETeams): enum edited

---
+++ src/main/java/cz/minelife/teams/ETeams.java
@@ -0,0 +1,40 @@
+package cz.minelife.teams;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+public enum ETeams {
+    ONELIVE("oneLive", 1),
+    TWOLIVES("twoLives", 2),
+    THREELIVES("threeLives", 3),
+    FOURLIVES("fourLives", 4),
+    FIVELIVES("fiveLives", 5),
+    SIXLIVES("sixLives", 6);
+
+    private int intLives;
+    private String teamName;
+    private Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+
+    ETeams(String teamName, int intLives) {
+        this.teamName = teamName;
+        this.intLives = intLives;
+    }
+
+    public Team getTeam() {
+        return scoreboard.getTeam(teamName);
+    }
+
+    public void setTeam(Player p) {
+        scoreboard.getTeam(teamName).addEntry(p.getDisplayName());
+    }
+
+    public int getIntLives() {
+        return intLives;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+}
