efd838f8226b957c07f77313501e02a91de69a72
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-24 12:12:24 +0100

    refactor(Teams): Added better access to teams

---
+++ src/main/java/cz/minelife/teams/Teams.java
@@ -0,0 +1,26 @@
+package cz.minelife.teams;
+
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
+
+public enum Teams {
+    ONELIVE("oneLive"),
+    TWOLIVES("twoLives"),
+    THREELIVES("threeLives"),
+    FOURLIVES("fourLives"),
+    FIVELIVES("fiveLives"),
+    SIXLIVES("sixLives");
+
+    private String lives;
+    private final Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+
+    Teams(String lives) {
+        this.lives = lives;
+    }
+
+    public Team getTeam() {
+        return scoreboard.getTeam(lives);
+    }
+}
