5f0ac25e8a3403d2a21c5e639f2d33d958b9b7c3
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-27 14:24:11 +0100

    fix(items): Items fixed

--- src/main/java/cz/minelife/commands/ItemCmd.java
+++ src/main/java/cz/minelife/commands/ItemCmd.java
@@ -13,18 +13,24 @@ public class ItemCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player p = (Player) sender;
-        if (args.length >= 2 && args[1] != null)
-            p = Bukkit.getPlayer(args[1]);
-        if (args[0].equalsIgnoreCase("onelive_stick")) {
-            p.getInventory().addItem(ONELIVE_STICK.getItem());
-            p.sendMessage("§7PŘEDMĚT BYL VYDÁN!");
-            return true;
-        } else if (args[0].equalsIgnoreCase("twolives_stick")) {
-            p.getInventory().addItem(TWOLIVES_STICK.getItem());
-            p.sendMessage("§7PŘEDMĚT BYL VYDÁN!");
-            return true;
+        if (sender.isOp() && args.length >= 1) {
+            if (args.length >= 2 && args[1] != null)
+                p = Bukkit.getPlayer(args[1]);
+
+            if (args[0].equalsIgnoreCase("onelive_stick")) {
+                p.getInventory().addItem(ONELIVE_STICK.getItem());
+                p.sendMessage("§7PŘEDMĚT BYL VYDÁN!");
+                return true;
+            } else if (args[0].equalsIgnoreCase("twolives_stick")) {
+                p.getInventory().addItem(TWOLIVES_STICK.getItem());
+                p.sendMessage("§7PŘEDMĚT BYL VYDÁN!");
+                return true;
+            }
+
+            sender.sendMessage("§cINVALID ITEM!");
+            return false;
         }
-        sender.sendMessage("§cINVALID ITEM!");
         return false;
     }
 }
+
