711625e9a52b2134a0de5ede9a4d237bff5ec9f6
Psbaldur <80245823+Psbaldur@users.noreply.github.com>
2022-01-30 20:55:17 +0100

    fix(Babice): Removed debug

--- src/main/java/cz/minelife/commands/StartSession.java
+++ src/main/java/cz/minelife/commands/StartSession.java
@@ -1,7 +1,5 @@
 package cz.minelife.commands;
 
-import cz.minelife.Main;
-import cz.minelife.boss.Boss;
 import cz.minelife.players.Boogeyman;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -9,12 +7,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class StartSession implements CommandExecutor {
-    private Main plugin;
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender.isOp()) {
-            new Boss();
             new Boogeyman().choose();
         } else {
             sender.sendMessage(ChatColor.RED + "Err");
